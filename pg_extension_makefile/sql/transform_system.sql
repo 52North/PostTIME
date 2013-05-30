5cf950073ecd5a7fbbb1149c1c34921355f71f26
ptrb <peterbrosseit@web.de>
2013-05-30 16:31:48 +0200

    added system-transformation-functions for the regular object types

--- pg_extension_makefile/sql/transform_system.sql
+++ pg_extension_makefile/sql/transform_system.sql
@@ -4,6 +4,9 @@ SELECT pt_transform_system('1970-1-1T0:0:0.001','TCS002');
 SELECT pt_transform_system('TCS0021','CAL001');
 SELECT pt_transform_system('TCS0021','TCS001');
 SELECT pt_transform_system('TCS0011','TCS003');
+/* regular types */
+SELECT pt_transform_system('R10/1970-1-1T0:1/PT1S','TCS002');
+SELECT pt_transform_system('TCS002R10/2/P1','CAL001');
 /* error */
 SELECT pt_transform_system('1970-1-1T0:0:0.001','ORD001');
 SELECT pt_transform_system('ORD001Triassic','TCS002');
