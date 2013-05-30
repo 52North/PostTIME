81147c7620dc9c44dd2fc763336a1cf6740f80e5
ptrb <peterbrosseit@web.de>
2013-05-30 15:00:56 +0200

    bug with tcs-regular types fixed

--- pg_extension_makefile/sql/regular_multi_to_multi.sql
+++ pg_extension_makefile/sql/regular_multi_to_multi.sql
@@ -2,3 +2,6 @@ SELECT pt_regular_multi_to_multi('R2/2000/P1Y/P9Y');
 SELECT pt_regular_multi_to_multi('R200/2000/P1Y/P9Y');
 SELECT pt_regular_multi_to_multi('R10/2013-4-30T12:12:59.999/P9Y8M7DT6H5M4.321S');
 SELECT pt_regular_multi_to_multi('R10/2013-4-30T12:12:59.999/P9Y8M7DT6H5M4.321S/PT12H22M59.123S');
+
+SELECT pt_regular_multi_to_multi('TCS002R10/1000.0000/P10/P1');
+SELECT pt_regular_multi_to_multi('TCS002R10/1000.0000/P10');
